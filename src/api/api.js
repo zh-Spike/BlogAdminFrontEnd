88708fd0b15fb243a1ba65eb340cca1ba23d0cf0
ETNJL <etnjl@outlook.com>
2020-08-27 21:47:04 +0800

    add&delete category

--- src/api/api.js
+++ src/api/api.js
@@ -10,5 +10,9 @@ export const doLogin = (verifyCode, captchaKey, user) => {
 };
 // 获取分类列表
 export const listCategories = () => {
-	return http.requestGet('/admin/category/list/' );
+	return http.requestGet('/admin/category/list/');
+};
+// 删除分类
+export const deleteCategoryById = (categoryId) => {
+	return http.requestDelete('/admin/category/' + categoryId);
 };
--- src/page/operation/manage-category.vue
+++ src/page/operation/manage-category.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="category-box">
 		<div class="category-action-bar">
-			<el-button type="primary" size="mini">添加分类</el-button>
+			<el-button type="primary" size="mini" @click="showAddCategory">添加分类</el-button>
 		</div>
 		<div class="category-list-box">
 			<el-table
@@ -17,6 +17,18 @@
 					prop="name"
 					label="分类名称"
 					width="120">
+				</el-table-column>
+				<el-table-column
+					label="状态"
+					width="200">
+					<template slot-scope="scope">
+						<div v-if="scope.row.status === '0'">
+							<el-tag type="danger">已删除</el-tag>
+						</div>
+						<div v-if="scope.row.status === '1'">
+							<el-tag type="success">正常</el-tag>
+						</div>
+					</template>
 				</el-table-column>
 				<el-table-column
 					prop="pinyin"
@@ -49,22 +61,51 @@
 					width="200">
 					<template slot-scope="scope">
 						<el-button type="primary" size="mini" @click="edit(scope.row)">编辑</el-button>
-						<el-button type="danger" size="mini" @click="deleteItem(scope.row)">删除</el-button>
+						<el-button type="danger" v-if="scope.row.status !== '0'" size="mini" @click="deleteItem(scope.row)">删除</el-button>
+						<el-button type="danger" v-if="scope.row.status === '0'" size="mini" @click="deleteItem(scope.row)" disabled>删除</el-button>
 					</template>
 				</el-table-column>
 			</el-table>
+		</div>
+		<div class="category-dialog-box">
+			<el-dialog
+				title="删除提示"
+				:visible.sync="deleteDialogShow"
+				width="400px">
+				<span>你确定要删除: {{ deleteMessage }} 这个分类吗？</span>
+				<span slot="footer" class="dialog-footer">
+                    <el-button size="mini" type="primary" @click="deleteDialogShow= false">取 消</el-button>
+					<el-button size="mini" type="danger" @click="doDeleteItem">确 定</el-button>
+				</span>
+			</el-dialog>
+			<el-dialog
+				:title="editTitle"
+				:visible.sync="editorDialogShow"
+				width="400px">
+				<div>这是一段信息</div>
+				<span slot="footer" class="dialog-footer">
+                    <el-button size="mini" type="danger" @click="editorDialogShow">取 消</el-button>
+					<el-button size="mini" type="primary" @click="editorDialogShow">{{editorCommitText}}</el-button>
+				</span>
+			</el-dialog>
 		</div>
 	</div>
 </template>
 
 <script>
-import {listCategories} from "@/api/api";
+import * as api from "@/api/api";
 import * as dateUtils from "@/utils/date";
 
 export default {
 	data() {
 		return {
-			categories: []
+			editorCommitText: '修改分类',
+			editorDialogShow: false,
+			editTitle: '编辑分类',
+			categories: [],
+			deleteDialogShow: false,
+			deleteMessage: '',
+			deleteTargetId: ''
 		}
 	},
 	methods: {
@@ -72,21 +113,44 @@ export default {
 			console.log(item);
 		},
 		deleteItem(item) {
-			console.log(item);
+			// 不是马上删除 弹出确认方框
+			this.deleteDialogShow = true;
+			this.deleteMessage = item.name;
+			this.deleteTargetId = item.id;
+			// console.log(item);
 		},
 		formatDate(dateStr) {
 			let date = new Date(dateStr);
 			return dateUtils.formatDate(date, 'yyyy-MM-dd hh:mm:ss');
+		},
+		doDeleteItem() {
+			api.deleteCategoryById(this.deleteTargetId).then(result => {
+				if (result.code === 10000) {
+					this.$message({
+						type: 'success',
+						center: true,
+						message: result.message
+					});
+					this.listCategories();
+				}
+			});
+			this.deleteDialogShow = false;
+		},
+		listCategories() {
+			api.listCategories().then(result => {
+				//	console.log(result);
+				if (result.code === 10000) {
+					this.categories = result.data;
+				}
+			});
+		},
+		showAddCategory() {
+			this.editorDialogShow = true;
 		}
 	},
 	mounted() {
 		// 去获取分类
-		listCategories().then(result => {
-			//	console.log(result);
-			if (result.code === 10000) {
-				this.categories = result.data;
-			}
-		})
+		this.listCategories();
 	}
 }
 </script>
